9dfd5538a5bc12b497e81639f9952a3ac17762f8
Diego <diegofreeman78@gmail.com>
2026-06-18 08:12:02 +0100

    Add user registration functionality and update user model; enhance main server routes

--- server/stream_movies_server/cmd/main.go
+++ server/stream_movies_server/cmd/main.go
@@ -13,6 +13,8 @@ func main() {
 	router.GET("/movies", controllers.GetMovies())
 	router.GET("/movies/:imdb_id", controllers.GetMovie())
 	router.POST("/movies", controllers.AddMovie())
+
+	router.POST("/users", controllers.RegisterUser())
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server")
 		panic(err)
---
+++ server/stream_movies_server/controllers/user_controllers.go
@@ -0,0 +1,112 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/DiegoFreema/stream_movies/database"
+	"github.com/DiegoFreema/stream_movies/models"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection("users")
+
+func HashPassword(password string) (string, error) {
+	HashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(HashPassword), nil
+}
+
+func RegisterUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		defer cancel()
+
+		var user models.User
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":   "Invalid user data",
+				"success": false,
+			})
+			return
+		}
+
+		validate := validator.New()
+
+		if err := validate.Struct(user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":   err.Error(),
+				"success": false,
+			})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error":   "Failed to check email",
+				"success": false,
+			})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusConflict, gin.H{
+				"error":   "Email already exists",
+				"success": false,
+			})
+			return
+		}
+
+		hashedPassword, err := HashPassword(user.Password)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error":   "Failed to hash password",
+				"success": false,
+			})
+			return
+		}
+		user.UserID = bson.NewObjectID().Hex()
+		user.CreatedAt = time.Now()
+		user.UpdatedAt = time.Now()
+		user.Password = hashedPassword
+
+		result, err := userCollection.InsertOne(ctx, user)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error":   "Failed to register user",
+				"success": false,
+			})
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{
+			"success": true,
+			"message": "User created",
+			"result":  result,
+		})
+
+	}
+}
+
+
+func LoginUser() gin.HandlerFunc{
+	return func(c *gin.Context){
+		
+	}
+}
